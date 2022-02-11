98a324354063543f8fc4711ec2b0ef424f122539
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-02-11 16:57:30 +0100

    Change javascript function on open mobile menu

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,32 @@ let hamburgerButton = document.querySelector(".header__nav__hamburger");
 let navMenu = document.querySelector(".header__nav__menu");
 let triangle = document.querySelector(".triangle__wrapper");
 
-const showMenu = () => {
+/* const showMenu = () => {
   if (navMenu.style.display === "flex") {
     navMenu.style.display = "none";
     triangle.style.display = "none";
   } else {
     navMenu.style.display = "flex";
     triangle.style.display = "flex";
+  }
+};
+
+hamburgerButton.addEventListener("click", showMenu); */
+
+
+const showMenu = () => {
+  if (navMenu.classList.contains("displayFlex")) {
+    navMenu.classList.remove("displayFlex");
+    triangle.classList.remove("displayFlex");
+
+    navMenu.classList.add("displayNone");
+    triangle.classList.add("displayNone");
+  } else {
+    navMenu.classList.remove("displayNone");
+    triangle.classList.remove("displayNone");
+
+    navMenu.classList.add("displayFlex");
+    triangle.classList.add("displayFlex");
   }
 };
 
