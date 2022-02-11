4144b9d116165debb02bb03c7c87ad91f93061b4
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-02-11 17:01:49 +0100

    Fix remove commented code

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,6 @@ let hamburgerButton = document.querySelector(".header__nav__hamburger");
 let navMenu = document.querySelector(".header__nav__menu");
 let triangle = document.querySelector(".triangle__wrapper");
 
-/* const showMenu = () => {
-  if (navMenu.style.display === "flex") {
-    navMenu.style.display = "none";
-    triangle.style.display = "none";
-  } else {
-    navMenu.style.display = "flex";
-    triangle.style.display = "flex";
-  }
-};
-
-hamburgerButton.addEventListener("click", showMenu); */
-
-
 const showMenu = () => {
   if (navMenu.classList.contains("displayFlex")) {
     navMenu.classList.remove("displayFlex");
